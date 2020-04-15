9331ae1fb09bf3f5b66fea8fe24f5b802c689499
Michael Lynch <michael@michaelrlynch.com>
2020-04-15 14:34:59 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,8 +1,10 @@
 class Helpers
 
-  def current_user
+  def current_user(session)
+    @user = User.find_by_id(sessions[:user_id])
   end
 
-  def is_logged_in?
+  def is_logged_in?(session)
+    !!session[:user_id] == User.find_by_id(sesions[:user_id])
   end
 end
